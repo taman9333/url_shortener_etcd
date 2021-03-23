334f7ebfcf472799cecdd1429bfb1d6f5512defc
taman9333 <taman9333@gmail.com>
2021-03-23 21:09:55 +0200

    Use etcd for getting & setting counter

--- counter.rb
+++ counter.rb
@@ -1,16 +1,39 @@
 # frozen_string_literal: true
 
+require_relative './etcd'
+
 # basic in-memory counter
 module Counter
+  COUNTER_RANGE = 1_000_000
+  KEY_NAME = 'current_counter'
+
   def self.init
-    @count = 0
+    current_counter = Etcd.conn.get(KEY_NAME)
+    if current_counter.kvs.empty?
+      Etcd.conn.put(KEY_NAME, COUNTER_RANGE.to_s)
+      @count = 0
+    else
+      @count = current_counter.kvs.first.value.to_i
+    end
+    @max = @count + COUNTER_RANGE
+    Etcd.conn.put(KEY_NAME, @max.to_s)
   end
 
   def self.increment
+    set_new_counter if !@count < @max
     @count += 1
   end
 
   def self.count
     @count
+  end
+
+  private
+
+  def set_new_counter
+    current_counter = Etcd.conn.get(KEY_NAME).kvs.first.value.to_i
+    @count = current_counter
+    @max = @count + COUNTER_RANGE
+    Etcd.conn.put(KEY_NAME, @max.to_s)
   end
 end
